8dbae03cd3461e91a8f38f7c5e2607005440e278
lCherS <lucascherez.cl@gmail.com>
2021-11-05 15:50:36 -0300

    Resolvido Ex2

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,27 @@
 import express from "express";
+import swaggerUI from "swagger-ui-express";
 
 import { usersRoutes } from "./routes/users.routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
 app.use("/users", usersRoutes);
 
 export { app };
+
+// Create - name, email
+// List -
+// Show - id
+// TurnADM - id
+
+// - Informações gerais da API (nome, descrição, etc.);
+// - Rotas;
+// - Parâmetros;
+// - Corpo da Requisição;
+// - Respostas de sucesso;
+// - Respostas de erro;
+// - Exemplos.
